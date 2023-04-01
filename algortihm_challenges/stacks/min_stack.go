9a129b84ed2a1f0e03b5033c7ec2fe7459c49b7c
FernasFragas <fp.work.uni@gmail.com>
2023-04-01 21:54:22 +0100

    1st look at minstack

--- algortihm_challenges/stacks/min_stack.go
+++ algortihm_challenges/stacks/min_stack.go
@@ -1,22 +1,35 @@
 package stacks
 
 type MinStack struct {
+	items []int
 }
 
 func Constructor() MinStack {
-
+	return MinStack{}
 }
 
 func (this *MinStack) Push(val int) {
-
+	this.items = append(this.items, val)
 }
 
 func (this *MinStack) Pop() {
-
+	itemsLength := len(this.items)
+	if itemsLength == 0 {
+		return
+	}
+	lastIdx := itemsLength - 1
+	this.items = this.items[:lastIdx]
 }
 
 func (this *MinStack) Top() int {
-
+	itemsLength := len(this.items)
+	if itemsLength == 0 {
+		panic("stack is empty")
+	}
+	lastIdx := itemsLength - 1
+	value := this.items[lastIdx]
+	this.items = this.items[:lastIdx]
+	return value
 }
 
 func (this *MinStack) GetMin() int {
