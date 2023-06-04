f3391f091038644f42470411443f50a557e66419
FernasFragas <fp.work.uni@gmail.com>
2023-06-04 20:42:40 +0100

    Added comments to the code

--- algortihm_challenges/linked_list/merge_two_sorted_lists.go
+++ algortihm_challenges/linked_list/merge_two_sorted_lists.go
@@ -20,7 +20,7 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		}
 		return list1
 	}
-
+	//verifies if one of the list is empty if so return the other one
 	if list1 == nil {
 		return list2
 	} else if list2 == nil {
@@ -33,15 +33,17 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	head := &ListNode{}
 	curr := head
 	for list1 != nil && list2 != nil {
-		if list1.Val >= list2.Val {
+		// verifies which value is smaller and adds it to the list
+		if list1.Val >= list2.Val { // case list1 is bigger
 			curr.Next = list2
 			list2 = list2.Next
+			// verifies if the curr.next value is not nil, if it is, the next element will be the element of the other list
 			if curr.Next != nil {
 				curr = curr.Next
 			} else {
 				curr.Next = list1
 			}
-		} else {
+		} else { // case list2 is bigger
 			curr.Next = list1
 			list1 = list1.Next
 			if curr.Next != nil {
@@ -51,7 +53,7 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 			}
 		}
 	}
-
+	// verifies if there is some element that was not iterated if so, it adds it to the list
 	if list1 != nil {
 		curr.Next = list1
 	} else if list2 != nil {
